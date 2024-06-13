689f7fa1c5fed3e2c8241de6222cf3514b391e19
ConnorLBrown <cbrown30@una.edu>
2024-06-13 13:28:10 -0500

    "Added few more lines testing out different operators"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!";
+
+    cout << "Enter your name:";
+    string name;
+    cin >> name;
+    cout << "Hello, World! from " << endl;
+    cout << name;
     return 0;
 }
